36dc61dce2b772e59c5bd183fa8b4609430b108e
hexadivine <hexadivine@gmail.com>
2024-01-10 21:57:50 +0530

    Integrated Navbar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Navigation from "./components/Navigation";
 
 function App() {
-  return <>hi</>;
+    return (
+        <>
+            <Navigation />
+        </>
+    );
 }
 
 export default App;
