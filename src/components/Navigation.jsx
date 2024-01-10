bff19ca0039176c4bc9de958a5f8b34452ce05a5
hexadivine <hexadivine@gmail.com>
2024-01-10 21:57:09 +0530

    Added new Navbar component

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+export default function Navigation() {
+    return (
+        <nav>
+            <div className="logo">BRAND Logo</div>
+            <ul>
+                <li>Home</li>
+                <li>Location</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+            <button>Login</button>
+        </nav>
+    );
+}
