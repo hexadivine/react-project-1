4ce103b5eb5c90fc86c76f63fa1dcb2b7b833f6f
hexadivine <hexadivine@gmail.com>
2024-01-12 18:50:25 +0530

    integrated herosection

--- project2_contactus/src/App.jsx
+++ project2_contactus/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
+import HeroSection from "./components/HeroSection/HeroSection";
 
 function App() {
     return (
         <div>
             <Navigation />
+            <HeroSection />
         </div>
     );
 }
