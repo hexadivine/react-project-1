830364425cbd87c1bf174e343f7a8020d5863ac9
hexadivine <hexadivine@gmail.com>
2024-01-12 17:57:45 +0530

    added navigation component

---
+++ project2_contactus/src/components/Navigation.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Navigation() {
+    return (
+        <nav>
+            <div className="container">
+                <img src="/images/brand_logo.png" alt="brandlogo" />
+                <ul>
+                    <li>Home</li>
+                    <li>Location</li>
+                    <li>About</li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navigation;
