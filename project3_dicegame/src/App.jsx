0dd87c965d7894f5a5b7bb7a49c4f21016a7ad1c
hexadivine <hexadivine@gmail.com>
2024-01-15 13:14:39 +0530

    updated imports

--- project3_dicegame/src/App.jsx
+++ project3_dicegame/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
-import HomePage from "./Components/HomePage";
-import GamePage from "./Components/GamePage";
+import HomePage from "./pages/HomePage";
+import GamePage from "./pages/GamePage";
 
 function App() {
     const [startGame, setStartGame] = useState(false);
