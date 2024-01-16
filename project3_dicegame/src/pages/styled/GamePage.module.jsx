3b0923c4d807a8a2d67c5bba30e5dbb8f9ef85fd
hexadivine <hexadivine@gmail.com>
2024-01-16 14:39:13 +0530

    added styled component for gamepage

--- project3_dicegame/src/pages/styled/GamePage.module.jsx
+++ project3_dicegame/src/pages/styled/GamePage.module.jsx
@@ -6,3 +6,21 @@ export const TopBar = styled.div`
     min-width: 1280px;
     margin: 64px 80px 0;
 `;
+
+export const GameContainer = styled.div`
+    max-width: 1280px;
+    height: calc(100vh - ${(props) => props.$heightoffset});
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+
+    Button {
+        margin-bottom: 15px;
+    }
+
+    p {
+        margin: 20px 0;
+    }
+`;
