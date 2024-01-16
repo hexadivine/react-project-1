5daf9a2f0649bd08ef463374163afd1f6c62e340
hexadivine <hexadivine@gmail.com>
2024-01-16 10:53:31 +0530

    updated Button component to work dynamically with props

--- project3_dicegame/src/styled/Button.module.jsx
+++ project3_dicegame/src/styled/Button.module.jsx
@@ -3,19 +3,29 @@ import styled from "styled-components";
 export const Button = styled.button`
     all: unset;
     padding: 10px 73px;
+    border: 2px solid black;
     border-radius: 5px;
-    background-color: black;
-    color: white;
+
     float: right;
-    border: 2px solid transparent;
 
-    transition: 0.4 background ease-out;
+    transition: ${(props) =>
+        props.$animate ? "0.4 background ease-out;" : ""};
 
-    &:hover {
-        background: transparent;
-        color: black;
-        border: 2px solid black;
+    ${(props) =>
+        props.$type === "primary"
+            ? `background-color: black;
+        color: white`
+            : `background-color: transparent;
+        color: black`};
 
-        transition: 0.3 background ease-in;
-    }
+    ${(props) =>
+        props.$animation === "true" && props.$type === "primary"
+            ? `
+        &:hover {
+            background: transparent;
+            color: black;
+            border: 2px solid black;
+            transition: 0.3 background ease-in;
+        }`
+            : ""};
 `;
