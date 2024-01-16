837b9e3ca39c8b302ffc64faec8181c74e661e65
hexadivine <hexadivine@gmail.com>
2024-01-16 11:01:47 +0530

    imported required functions and used states and props to build game logic

--- project3_dicegame/src/pages/GamePage.jsx
+++ project3_dicegame/src/pages/GamePage.jsx
@@ -1,19 +1,67 @@
 import Scorecard from "../components/Scorecard";
 import NumberSelector from "../components/NumberSelector";
-import { TopBar } from "./styled/GamePage.module";
-import { Container } from "../styled/Container.module";
-// import { Button } from "../styled/Button.module";
+import { TopBar, GameContainer } from "./styled/GamePage.module";
+import { dices } from "../config/dices.module";
+import { Button } from "../styled/Button.module";
+import { useState } from "react";
+import GameRules from "../components/GameRules";
 
 function GamePage(props) {
+    const [randomDiceNum, setRandomDiceNum] = useState(1);
+    const [selectedDiceNum, setSelectedDiceNum] = useState();
+    const [errorMsg, setErrorMsg] = useState("");
+    const [totalScore, setTotalScore] = useState(0);
+    const [maxScore, setMaxScore] = useState(0);
+    const [showRules, setShowRules] = useState(false);
+
+    const randomDice = (min, max) => {
+        if (selectedDiceNum === undefined) {
+            setErrorMsg("You have not selected any number");
+            return;
+        } else setErrorMsg("");
+
+        const randomNumber = Math.floor(Math.random() * (max - min) + min);
+        setRandomDiceNum(randomNumber);
+
+        if (selectedDiceNum === randomNumber)
+            setTotalScore((prev) => prev + 10);
+        else setTotalScore((prev) => prev - 2);
+
+        if (totalScore > maxScore) setMaxScore(totalScore);
+        setSelectedDiceNum(undefined);
+    };
+
     return (
         <>
             <TopBar>
-                <Scorecard />
-                <NumberSelector />
+                <Scorecard totalScore={totalScore} maxScore={maxScore} />
+                <NumberSelector
+                    errorMsg={errorMsg}
+                    setSelectedDiceNum={setSelectedDiceNum}
+                    selectedDiceNum={selectedDiceNum}
+                />
             </TopBar>
-            <Container heightOffset={"220px"}>
-                <img src="" alt="" onClick={}/>
-            </Container>
+            <GameContainer $heightoffset={"184px"} $flexdirection={"column"}>
+                <img
+                    onClick={() => randomDice(1, 7)}
+                    src={dices[randomDiceNum - 1]}
+                    alt="dice"
+                    height={"100px"}
+                    width={"100px"}
+                />
+                <p>Click on Dice to roll</p>
+                <Button
+                    onClick={() => setTotalScore(0)}
+                    $type="primary"
+                    $animation="true"
+                >
+                    Reset Score
+                </Button>
+                <Button onClick={() => setShowRules(!showRules)}>
+                    {showRules ? "Hide" : "Show"} Rules
+                </Button>
+                {showRules ? <GameRules /> : ""}
+            </GameContainer>
         </>
     );
 }
